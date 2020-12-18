378635ff475f9d47168c87218d5b390ac4267b37
xuweisong <xuweisong37@126.com>
2020-12-18 14:31:31 +0800

    database.test.php

---
+++ release_test.php
@@ -0,0 +1,11 @@
+<?php
+
+    $command = "git checkout master && git pull";
+	(exec($command,$return));
+	echo implode("\n",$return)."\n";
+	unset($return);
+	$command = "copy config/database.test.php config/database.php";
+	(exec($command,$return));
+	echo implode("\n",$return)."\n";
+	unset($return);
+	
