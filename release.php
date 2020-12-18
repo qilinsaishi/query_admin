84a3df3708ce3725a79b19e7c7143545dccf0da0
xiaodong chen <cxd032404@hotmail.com>
2020-12-18 14:25:36 +0800

    merge

---
+++ release.php
@@ -0,0 +1,10 @@
+<?php
+
+$command = "git checkout master && git pull";
+(exec($command,$return));
+echo implode("\n",$return)."\n";
+unset($return);
+$command = "cp config/database.master.php config/database.php";
+(exec($command,$return));
+echo implode("\n",$return)."\n";
+unset($return);
