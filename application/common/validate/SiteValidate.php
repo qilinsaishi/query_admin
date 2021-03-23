37520d992f87c0d1abcef074e5ce77fa992c817a
xuweisong <xuweisong37@126.com>
2021-03-23 14:49:39 +0800

    去掉url验证

--- application/common/validate/SiteValidate.php
+++ application/common/validate/SiteValidate.php
@@ -7,13 +7,13 @@ class SiteValidate extends Validate
 {
     protected $rule = [
         'name'          => 'require',
-        'domain' => 'require|url',
+        'domain' => 'require',
     ];
 
     protected $message = [
         'name.require'   => '请输入站点名称',
         'domain.require' => '站点域名必须填写',
-        'domain.url'     => '站点域名格式不正确',
+        //'domain.url'     => '站点域名格式不正确',
     ];
 
     protected $scene = [
